4c02796dde240e77b8b7e82fc3efeaca31242b9a
Claudio Paredes <claudioparedesarbelo@gmail.com>
2023-08-16 14:15:31 -0300

    Trabajando en el Cart

--- src/dao/mongoManager/models/cart.model.js
+++ src/dao/mongoManager/models/cart.model.js
@@ -3,12 +3,7 @@ import mongoose from "mongoose";
 const cartCollection = 'cart'
 
 const cartSchema = new mongoose.Schema({
-    marca: {type: String, require: true},
-    description: {type: String, require: true},
-    code: {type: String, require: true},
-    price: {type: Number, require: true},
-    stock: {type: Number, require: true},
-    thumbnails: {type: String, require: true},
+    product
     
 })
 
